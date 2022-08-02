8e40f4b0b6a413f17e68fcda5443a26d8d2ec664
Jarosz Wojciech <jaroszw@gmail.com>
2022-08-02 14:23:40 +0200

    sending email

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,7 @@
 const Users = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const sendMail = require("./sendMail");
 
 const CLIENT_URL = process.env.CLIENT_URL;
 
